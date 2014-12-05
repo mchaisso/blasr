9acc582733c7901d6a28b39e69e5908e61117f22
Mark Chaisson <mchaisso@uw.edu>
2014-12-05 08:57:19 -0800

    Made -ldl standard for all code linking to hdf

--- common.mk
+++ common.mk
@@ -11,7 +11,8 @@ HDF5LIB    = hdf5
 HDF5LIBCPP = hdf5_cpp
 LINK_PROFILER = 
 GCCOPTS = -g -O3  -Wno-div-by-zero $(INCLUDEDIRS) -fpermissive -static 
-
+#
+HDF_REQ_LIBS=-lz -lpthread -ldl
 CPPOPTS = $(GCCOPTS) $(INCLUDEDIRS)
 CCOPTS  = $(GCCOPTS) $(INCLUDEDIRS)  
 CPP = g++
