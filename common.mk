7935bf64b05770d794cf95a1412e6adc7f7dc577
Mark Chaisson <mchaisso@gmail.com>
2018-03-22 13:59:38 -0700

    Fixed hardcoding of

--- common.mk
+++ common.mk
@@ -1,9 +1,9 @@
 #
 # Definitions common to all make files.
 #
-
-HDF5INCLUDEDIR=/home/mchaisso/software/include
-HDF5LIBDIR=/home/mchaisso/software/lib
+# This should point to HDF v1.8.+
+HDF5INCLUDEDIR?=/home/mchaisso/software/include
+HDF5LIBDIR?=/home/mchaisso/software/lib
 
 INCLUDEDIRS = -I $(PBCPP_DIR)/common -I $(HDF5INCLUDEDIR) 
 
