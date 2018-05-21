7698d0794bc09b9a6ae58b376f4edd40d02cf746
Mark Chaisson <mchaisso@uw.edu>
2018-05-21 00:03:08 -0700

    Fixes for c++11 build. 	modified:   common.mk 	modified:   pbihdfutils/LoadPulses.cpp 	modified:   samutils/Makefile

--- common.mk
+++ common.mk
@@ -10,7 +10,7 @@ INCLUDEDIRS = -I $(PBCPP_DIR)/common -I $(HDF5INCLUDEDIR)
 HDF5LIB    = hdf5
 HDF5LIBCPP = hdf5_cpp
 LINK_PROFILER = 
-GCCOPTS = -O3 -Wno-div-by-zero $(INCLUDEDIRS) -fpermissive -mtune=native
+GCCOPTS = -w -O3 -Wno-div-by-zero $(INCLUDEDIRS) -fpermissive -mtune=native
 
 HDF_REQ_LIBS= -lz -lpthread -ldl 
 CPPOPTS = $(GCCOPTS) $(INCLUDEDIRS) 
