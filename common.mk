912e0751b1b5eddfaf1eec61fdccd42ed27ff44f
Mark Chaisson <mchaisso@uw.edu>
2018-05-21 00:14:17 -0700

    Fixed common 	modified:   common.mk

--- common.mk
+++ common.mk
@@ -2,15 +2,10 @@
 # Definitions common to all make files.
 #
 
-<<<<<<< HEAD
 HDF5INCLUDEDIR ?= /usr/include
 HDF5LIBDIR     ?= /usr/lib
-HDF5INCLUDEDIR?=/net/eichler/vol5/home/mchaisso/software/hdf/include/
-HDF5LIBDIR?=/net/eichler/vol5/home/mchaisso/software/hdf/lib
-=======
-HDF5INCLUDEDIR?=/home/mchaisso/software/include
-HDF5LIBDIR?=/home/mchaisso/software/lib
->>>>>>> 3572cfb26594ed07750e1781e6ef9f2ff4356583
+HDF5INCLUDEDIR?=/usr/include
+HDF5LIBDIR?=/usr/lib
 
 INCLUDEDIRS = -I $(PBCPP_DIR)/common -I $(HDF5INCLUDEDIR) 
 
